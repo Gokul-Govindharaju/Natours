26b2c4c60d5a2bbf7fb91ef9f2d164eb5b7cece5
Gokul-Govindharaju <gokulgovindh04@gmail.com>
2024-04-28 20:09:08 +0530

    removed some comments

--- app.js
+++ app.js
@@ -108,26 +108,7 @@ app.use((req, res, next) => {
 
 // ROUTE HANDLERS
 
-//ROUTES
-
-// app.get('/api/v1/tours', getAllTours)
-// app.get('/api/v1/tours/:id',getTour)
-
-// app.post('/api/v1/tours',createTour)
-
-// app.patch('/api/v1/tours/:id',updataTour)
-// app.delete('/api/v1/tours/:id',deleteTour)
-
-// app
-// .route('/api/v1/tours')
-// .get(getAllTours)
-// .post(createTour);
-
-// app
-// .route('/api/v1/tours/:id')
-// .get(getTour)
-// .patch(updataTour)
-// .delete(deleteTour);
+//ROUTESs
 
 app.use('/',viewRouter)
 app.use('/api/v1/tours', tourRouter);
