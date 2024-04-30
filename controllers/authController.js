af4b501e8cb2a8c64417f5d35ad61f850b389aed
Gokul-Govindharaju <gokulgovindh04@gmail.com>
2024-04-30 09:28:17 +0530

    Final commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,19 +12,17 @@ const signToken = (id) => {
   });
 };
 
-const createSendToken = (user, statusCode, res) => {
+const createSendToken = (user, statusCode,req, res) => {
   const token = signToken(user._id);
-  const cookieOptions = {
+  res.cookie('jwt', token,  {
     expires: new Date(
       Date.now() +
         Number(process.env.JWT_COOKIE_EXPIRES_IN) * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-  };
+    secure: req.secure || req.headers('x-forwarded-proto') === 'https'
+  });
 
-  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
-
-  res.cookie('jwt', token, cookieOptions);
   user.password = undefined;
   res.status(statusCode).json({
     status: 'success',
@@ -47,7 +45,7 @@ exports.signup = catchAsync(async (req, res, next) => {
 
   await new Email(newUser, url).sendWelcome();
 
-  createSendToken(newUser, 201, res);
+  createSendToken(newUser, 201,req, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -65,7 +63,7 @@ exports.login = catchAsync(async (req, res, next) => {
   }
 
   //    3)if everthing ok, send token to client
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req,res);
 });
 
 exports.logout = (req, res) => {
@@ -228,7 +226,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   // 3)Update changedPasswordAt property for the user
 
   // 4)Log the user in ,send JWT
-  createSendToken(user, 200, res);
+  createSendToken(user, 200,req, res);
 });
 
 exports.updatePassword = catchAsync(async (req, res, next) => {
@@ -251,5 +249,5 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
   await user.save();
 
   // 4)4Log user in ,send jwt
-  createSendToken(user, 200, res);
+  createSendToken(user, 200,req, res);
 });
